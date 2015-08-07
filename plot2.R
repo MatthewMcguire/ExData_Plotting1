00845497646f5a25fefa6d340221fef470abbe8e
Matthew McGuire <mattmcg@gmail.com>
2015-08-07 17:03:28 -0400

    Adding plot images and code

---
+++ plot2.R
@@ -0,0 +1,31 @@
+# setwd("~/Dropbox/__ summer 2015/_Exploratory Data Analysis/project 1")
+
+# use lubridate package for easy date/time parsing
+require(lubridate)
+
+# read full hpc data set
+hpcFile <- "household_power_consumption.txt"
+hpcColClasses <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric" )
+hpc <- read.table(file=hpcFile, header=T, sep=";", na.strings = "?", colClasses = hpcColClasses)
+
+# Convert Date and Time values to POSIXct
+hpc$DateTime <- dmy_hms(paste(hpc[,1], hpc[,2]))
+hpc$Date <- dmy(hpc$Date)
+hpc$Time <- hms(hpc$Time)
+
+# select the data for 2/1/2007 and 2/2/2007 and place in a subset
+inSelection <- (year(hpc$Date) == 2007 & month(hpc$Date) == 2 & (day(hpc$Date) %in% c(1,2)))
+hpcSelection <- hpc[inSelection,]
+
+# remove the huge hpc data file form memory
+rm(hpc)
+rm(inSelection)
+
+# open a graphic device to a png file
+png(filename = "plot2.png",width = 500, height = 500, units = "px", pointsize = 12, bg = "white")
+
+# create line plot
+plot(x=hpcSelection$DateTime, y=hpcSelection$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+# close the png graphics device
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,46 @@
+# setwd("~/Dropbox/__ summer 2015/_Exploratory Data Analysis/project 1")
+
+# use lubridate package for easy date/time parsing
+require(lubridate)
+
+# read full hpc data set
+hpcFile <- "household_power_consumption.txt"
+hpcColClasses <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric" )
+hpc <- read.table(file=hpcFile, header=T, sep=";", na.strings = "?", colClasses = hpcColClasses)
+
+# Convert Date and Time values to POSIXct
+hpc$DateTime <- dmy_hms(paste(hpc[,1], hpc[,2]))
+hpc$Date <- dmy(hpc$Date)
+hpc$Time <- hms(hpc$Time)
+
+# select the data for 2/1/2007 and 2/2/2007 and place in a subset
+inSelection <- (year(hpc$Date) == 2007 & month(hpc$Date) == 2 & (day(hpc$Date) %in% c(1,2)))
+hpcSelection <- hpc[inSelection,]
+
+# remove the huge hpc data file form memory
+rm(hpc)
+rm(inSelection)
+
+# open a graphic device to a png file
+png(filename = "plot4.png",width = 500, height = 500, units = "px", pointsize = 12, bg = "white")
+
+# set up a panel of four plots
+par(mfrow = c(2, 2))
+
+# create first line plot
+plot(x=hpcSelection$DateTime, y=hpcSelection$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+# create second line plot
+plot(x=hpcSelection$DateTime, y=hpcSelection$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+# create third plot
+plot(x=hpcSelection$DateTime, y=hpcSelection$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
+lines(x=hpcSelection$DateTime, y=hpcSelection$Sub_metering_2, col="red")
+lines(x=hpcSelection$DateTime, y=hpcSelection$Sub_metering_3,  col="blue")
+legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# create fourth line plot
+plot(x=hpcSelection$DateTime, y=hpcSelection$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+# close the png graphics device
+dev.off()
